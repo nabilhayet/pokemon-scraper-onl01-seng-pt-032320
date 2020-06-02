05ba5aff9956c786b0fd2742052044722c3c6af1
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-02 02:57:53 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -8,13 +8,8 @@ class Pokemon
     @db = db
   end
 
-  def self.save(name,type,db)
-    sql = <<-SQL
-    INSERT INTO pokemon (name, type, db)
-    VALUES (?, ?, ?)
-    SQL
-
-    DB[:conn].execute(sql, self.name, self.type, self.db)
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end 
 
   def self.find_by_id(id)
